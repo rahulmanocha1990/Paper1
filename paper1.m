109ddb8d05fd66973a0c03e259788f31ed173204
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-01-31 13:00:01 -0600

    exp1
    paper1

---
+++ paper1.m
@@ -0,0 +1,8 @@
+%Paper 1 Fractal Dimension Based ,OLGA SOURINA, YISI LIU
+% http://www.ntu.edu.sg/home/eosourina/Papers/OSBIOSIGNALS_66_CR.pdf
+% Channel AF3 for arousal level classification
+% Channel F4 and FC6 for valence level
+% sliding window, 99% overlap
+% SVM poly kernel size 5
+
+%% 
