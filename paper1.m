b4a70216bac6fac7e8374e9c121ca099e6d96ff6
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-02-01 13:44:03 -0600

    1Feb

--- paper1.m
+++ paper1.m
@@ -1,8 +1,29 @@
 %Paper 1 Fractal Dimension Based ,OLGA SOURINA, YISI LIU
 % http://www.ntu.edu.sg/home/eosourina/Papers/OSBIOSIGNALS_66_CR.pdf
-% Channel AF3 for arousal level classification
-% Channel F4 and FC6 for valence level
+% Channel FC6 for arousal level classification
+% Channel F4 and AF3 for valence level
 % sliding window, 99% overlap
 % SVM poly kernel size 5
 %%
-%% 
+%labels has 1st column as Valence and second as Arousal
+%Test Arousal level classification . Arousal < 5 , class 0 , Arousal > 5
+%Class 1.
+% subject 1
+load ../data_matlab/s01.mat
+Arousal=labels(:,2);
+VideoClass0=find(Arousal<5);
+VideoClass1=find(Arousal>=5);
+Fs=128;
+channel={'FC6'};
+method='F';
+wlen=256;
+wshft=128;
+for i=1:length(VideoClass0)
+    X=squeeze(data(VideoClass0(i),:,:));
+    FeatureClass0(:,i)=FeatureGen(X,channel,method,Fs,wlen,wshft);
+end
+
+for i=1:length(VideoClass1)
+    X=squeeze(data(VideoClass1(i),:,:));
+    FeatureClass1(:,i)=FeatureGen(X,channel,method,Fs,wlen,wshft);
+end
