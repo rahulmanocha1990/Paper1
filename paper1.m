f90936be61f183f0cb37afc45b937f0619e567af
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-02-09 15:41:13 -0600

    minor change

--- paper1.m
+++ paper1.m
@@ -10,6 +10,7 @@
 %Class 1.
 % subject 1 
 %%
+%Collect all File names
 cd ../data_matlab
 Direc=dir('*.mat');
 for i=1:length(Direc)
@@ -18,7 +19,7 @@ end
 cd ../common
 %%
 %Arousal Level Recognition with channel FC6, 1 Fractal Feature , SVM Poly
-ExperimentId='../Paper1/Exp_1_Feb_2015.txt';
+ExperimentId='../Paper1/Exp_1_Feb_2015.csv';
 fid=fopen(ExperimentId,'w');
 for filenum=1:size(MatFiles,1)
   name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
@@ -45,10 +46,11 @@ for filenum=1:size(MatFiles,1)
    [B,C,O]=Classifier(Data,'svm',0.7,4);
    clear data
    clear labels
-   fprintf(fid,'Subject%d \n',filenum);
-   fprintf(fid,'Accuracy=%f \n',B);
-   fprintf(fid,'ConfMat= \n');
-   fprintf(fid,'%f %f \n',C);
+   fprintf(fid,'Subject%d\n',filenum);
+   fprintf(fid,'Accuracy,%f\n',B);
+   fprintf(fid,'ConfMat\n');
+   fprintf(fid,'%f,%f\n',C(1,:));
+   fprintf(fid,'%f,%f\n',C(2,:));
 end
 fclose(fid);
 
