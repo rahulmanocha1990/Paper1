8c9a0bd04b1f7c377e26d32a802aafda52fb5d0f
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-03-07 13:36:48 -0600

    paper5

--- paper5.m
+++ paper5.m
@@ -17,6 +17,7 @@ for i=1:length(Direc)
 MatFiles(i,:)=Direc(i).name;%get all file names
 end
 cd ../common
+epoch=[385,8064];
 %%
 % Try 2 emotions PHH and NLL (Valence>5,Arousal>5,Dominance>5 ; Valence<5,Arousal<5,Dominance<5)
 channels={'FC5','F4','F7','AF3'};
@@ -38,12 +39,12 @@ for filenum=1:size(MatFiles,1)
   wlen=512;
   wshft=128;
   for i=1:length(VideoClass0)
-     X=squeeze(data(VideoClass0(i),:,1:8096-384));
+     X=squeeze(data(VideoClass0(i),:,epoch(1):epoch(2)));
      FeatureClass0=[FeatureClass0;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end 
 
   for i=1:length(VideoClass1)
-     X=squeeze(data(VideoClass1(i),:,1:8096-384));
+     X=squeeze(data(VideoClass1(i),:,epoch(1):epoch(2)));
      FeatureClass1=[FeatureClass1;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end
   outfile=sprintf('../Paper1/Features/%s.mat',name(16:end-4));
@@ -64,51 +65,51 @@ end
 %fclose(fid);
 
 %%
-clear all;
-cd ../Paper1/Features
-Direc=dir('*.mat');
-for i=1:length(Direc)
-FeatFiles(i,:)=Direc(i).name;%get all file names
-end
-cd ../../common
+% clear all;
+% cd ../Paper1/Features
+% Direc=dir('*.mat');
+% for i=1:length(Direc)
+% FeatFiles(i,:)=Direc(i).name;%get all file names
+% end
+% cd ../../common
 
 %%
 %Load saved Features and classify them with SVM
 % normalise each feature type across 4 channels
-ExperimentId='../Paper1/Exp_10_Feb_2015_class2.csv';
-fid=fopen(ExperimentId,'w');
-fprintf(fid,'Paper5\n');
-
-for filenum=1:size(FeatFiles,1)
-    name=sprintf('../Paper1/Features/%s',FeatFiles(filenum,:));
-    load(name);
-    for i=1:size(FeatureClass0,1)
-        A=[];
-         for j=1:size(FeatureClass0,2)
-                 A=[A squeeze(FeatureClass0(i,j,:))'./norm(squeeze(FeatureClass0(i,j,:)))];
-         end
-         FC0(i,:)=A;
-    end
-    for i=1:size(FeatureClass1,1)
-        A=[];
-         for j=1:size(FeatureClass1,2)
-                 A=[A squeeze(FeatureClass1(i,j,:))'./norm(squeeze(FeatureClass1(i,j,:)))];
-         end
-         FC1(i,:)=A;
-    end
-    %SVM classification
-   Data=[[zeros(size(FC0,1),1) FC0];[ones(size(FC1,1),1) FC1]];
-   [B,C,O]=Classifier(Data,'svm',0.7,4);
-   clear FeatureClass0
-   clear FeatureClass1
-   fprintf(fid,'Subject%d\n',filenum);
-   fprintf(fid,'Accuracy,%f\n',B);
-   fprintf(fid,'ConfMat\n');
-   fprintf(fid,'%f,%f\n',C(1,:));
-   fprintf(fid,'%f,%f\n',C(2,:));
-  
-end
-fclose(fid);
+% ExperimentId='../Paper1/Exp_10_Feb_2015_class2.csv';
+% fid=fopen(ExperimentId,'w');
+% fprintf(fid,'Paper5\n');
+% 
+% for filenum=1:size(FeatFiles,1)
+%     name=sprintf('../Paper1/Features/%s',FeatFiles(filenum,:));
+%     load(name);
+%     for i=1:size(FeatureClass0,1)
+%         A=[];
+%          for j=1:size(FeatureClass0,2)
+%                  A=[A squeeze(FeatureClass0(i,j,:))'./norm(squeeze(FeatureClass0(i,j,:)))];
+%          end
+%          FC0(i,:)=A;
+%     end
+%     for i=1:size(FeatureClass1,1)
+%         A=[];
+%          for j=1:size(FeatureClass1,2)
+%                  A=[A squeeze(FeatureClass1(i,j,:))'./norm(squeeze(FeatureClass1(i,j,:)))];
+%          end
+%          FC1(i,:)=A;
+%     end
+%     %SVM classification
+%    Data=[[zeros(size(FC0,1),1) FC0];[ones(size(FC1,1),1) FC1]];
+%    [B,C,O]=Classifier(Data,'svm',0.7,4);
+%    clear FeatureClass0
+%    clear FeatureClass1
+%    fprintf(fid,'Subject%d\n',filenum);
+%    fprintf(fid,'Accuracy,%f\n',B);
+%    fprintf(fid,'ConfMat\n');
+%    fprintf(fid,'%f,%f\n',C(1,:));
+%    fprintf(fid,'%f,%f\n',C(2,:));
+%   
+% end
+% fclose(fid);
 
 
 %%
@@ -136,21 +137,21 @@ for filenum=1:size(MatFiles,1)
   wlen=512;
   wshft=128;
   for i=1:length(VideoClass0)
-     X=squeeze(data(VideoClass0(i),:,1:8096-384));
+     X=squeeze(data(VideoClass0(i),:,epoch(1):epoch(2)));
      FC0=[FC0;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end 
 
   for i=1:length(VideoClass1)
-     X=squeeze(data(VideoClass1(i),:,1:8096-384));
+     X=squeeze(data(VideoClass1(i),:,epoch(1):epoch(2)));
      FC1=[FC1;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end
   for i=1:length(VideoClass2)
-     X=squeeze(data(VideoClass2(i),:,1:8096-384));
+     X=squeeze(data(VideoClass2(i),:,epoch(1):epoch(2)));
      FC2=[FC2;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end 
 
   for i=1:length(VideoClass3)
-     X=squeeze(data(VideoClass3(i),:,1:8096-384));
+     X=squeeze(data(VideoClass3(i),:,epoch(1):epoch(2)));
      FC3=[FC3;FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8,'HOC',5,'STAT','msdefg')];
   end
   
@@ -173,73 +174,73 @@ end
 %fclose(fid);
 
 %%
-clear all;
-cd ../Paper1/Features_4class
-Direc=dir('*.mat');
-for i=1:length(Direc)
-FeatFiles(i,:)=Direc(i).name;%get all file names
-end
-cd ../../common
-
-%%
-ExperimentId='../Paper1/Exp_10_Feb_2015_class4.csv';
-fid=fopen(ExperimentId,'w');
-fprintf(fid,'Paper5\n');
-
-for filenum=1:size(FeatFiles,1)
-    name=sprintf('../Paper1/Features_4class/%s',FeatFiles(filenum,:));
-    load(name);
-    if(~isempty(FC0) && ~isempty(FC1) && ~isempty(FC2) && ~isempty(FC3))
-    for i=1:size(FC0,1)
-        A=[];
-         for j=1:size(FC0,2)
-                 A=[A squeeze(FC0(i,j,:))'./norm(squeeze(FC0(i,j,:)))];
-         end
-         FV0(i,:)=A;
-    end
-    for i=1:size(FC1,1)
-        A=[];
-         for j=1:size(FC1,2)
-                 A=[A squeeze(FC1(i,j,:))'./norm(squeeze(FC1(i,j,:)))];
-         end
-         FV1(i,:)=A;
-    end
-    for i=1:size(FC2,1)
-        A=[];
-         for j=1:size(FC2,2)
-                 A=[A squeeze(FC2(i,j,:))'./norm(squeeze(FC2(i,j,:)))];
-         end
-         FV2(i,:)=A;
-    end
-    for i=1:size(FC3,1)
-        A=[];
-         for j=1:size(FC3,2)
-                 A=[A squeeze(FC3(i,j,:))'./norm(squeeze(FC3(i,j,:)))];
-         end
-         FV3(i,:)=A;
-    end
-    %SVM classification
-   Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
-   [B,C,O]=Classifier(Data,'svm',0.7,4);
-
-   fprintf(fid,'Subject%d\n',filenum);
-   fprintf(fid,'Accuracy,%f\n',B);
-   fprintf(fid,'ConfMat\n');
-     for con=1:4
-         fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
-     end
-    end
-   clear FC0
-   clear FC1
-   clear FC2
-   clear FC3
-end
-fclose(fid);
+% clear all;
+% cd ../Paper1/Features_4class
+% Direc=dir('*.mat');
+% for i=1:length(Direc)
+% FeatFiles(i,:)=Direc(i).name;%get all file names
+% end
+% cd ../../common
+% 
+% %%
+% ExperimentId='../Paper1/Exp_10_Feb_2015_class4.csv';
+% fid=fopen(ExperimentId,'w');
+% fprintf(fid,'Paper5\n');
+% 
+% for filenum=1:size(FeatFiles,1)
+%     name=sprintf('../Paper1/Features_4class/%s',FeatFiles(filenum,:));
+%     load(name);
+%     if(~isempty(FC0) && ~isempty(FC1) && ~isempty(FC2) && ~isempty(FC3))
+%     for i=1:size(FC0,1)
+%         A=[];
+%          for j=1:size(FC0,2)
+%                  A=[A squeeze(FC0(i,j,:))'./norm(squeeze(FC0(i,j,:)))];
+%          end
+%          FV0(i,:)=A;
+%     end
+%     for i=1:size(FC1,1)
+%         A=[];
+%          for j=1:size(FC1,2)
+%                  A=[A squeeze(FC1(i,j,:))'./norm(squeeze(FC1(i,j,:)))];
+%          end
+%          FV1(i,:)=A;
+%     end
+%     for i=1:size(FC2,1)
+%         A=[];
+%          for j=1:size(FC2,2)
+%                  A=[A squeeze(FC2(i,j,:))'./norm(squeeze(FC2(i,j,:)))];
+%          end
+%          FV2(i,:)=A;
+%     end
+%     for i=1:size(FC3,1)
+%         A=[];
+%          for j=1:size(FC3,2)
+%                  A=[A squeeze(FC3(i,j,:))'./norm(squeeze(FC3(i,j,:)))];
+%          end
+%          FV3(i,:)=A;
+%     end
+%     %SVM classification
+%    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
+%    [B,C,O]=Classifier(Data,'svm',0.7,4);
+% 
+%    fprintf(fid,'Subject%d\n',filenum);
+%    fprintf(fid,'Accuracy,%f\n',B);
+%    fprintf(fid,'ConfMat\n');
+%      for con=1:4
+%          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
+%      end
+%     end
+%    clear FC0
+%    clear FC1
+%    clear FC2
+%    clear FC3
+% end
+% fclose(fid);
 %%
 % USE only HOC Features and combination of HOC and STAT
 %%
 % Try 4 emotions PLL,PHH,NLL,NHH (V>5,A<5,D<5; V>5,A>5,D>5 ; V<5,A<5,D<5; V<5,A>5,D>5)
-channels={'FC5'};%,'F4','F7','AF3'};
+channels={'FC5','F4','F7','AF3'};
 Cind=channel2ind('../data_matlab/channels.txt',channels);%Indices of all channels
 %ExperimentId='../Paper1/Exp_10_Feb_2015.csv';
 %fid=fopen(ExperimentId,'w');
@@ -262,26 +263,26 @@ for filenum=1:size(MatFiles,1)
   wlen=512;
   wshft=128;
   for i=1:length(VideoClass0)
-     X=squeeze(data(VideoClass0(i),:,1:8096-384));
+     X=squeeze(data(VideoClass0(i),:,epoch(1):epoch(2)));
      FC0=[FC0;FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
   end 
 
   for i=1:length(VideoClass1)
-     X=squeeze(data(VideoClass1(i),:,1:8096-384));
+     X=squeeze(data(VideoClass1(i),:,epoch(1):epoch(2)));
      FC1=[FC1;FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
   end
   for i=1:length(VideoClass2)
-     X=squeeze(data(VideoClass2(i),:,1:8096-384));
+     X=squeeze(data(VideoClass2(i),:,epoch(1):epoch(2)));
      FC2=[FC2;FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
   end 
 
   for i=1:length(VideoClass3)
-     X=squeeze(data(VideoClass3(i),:,1:8096-384));
+     X=squeeze(data(VideoClass3(i),:,epoch(1):epoch(2)));
      FC3=[FC3;FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
   end
   
   
-  outfile=sprintf('../Paper1/Features_4class_stat_1chan/%s.mat',name(16:end-4));
+  outfile=sprintf('../Paper1/Features_4class_stat/%s.mat',name(16:end-4));
   save(outfile,'FC0','FC1','FC2','FC3');
   
   
@@ -300,69 +301,129 @@ end
 
 %%
 %clear all;
-cd ../Paper1/Features_4class
-Direc=dir('*.mat');
-for i=1:length(Direc)
-FeatFiles(i,:)=Direc(i).name;%get all file names
-end
-cd ../../common
-
+% cd ../Paper1/Features_4class
+% Direc=dir('*.mat');
+% for i=1:length(Direc)
+% FeatFiles(i,:)=Direc(i).name;%get all file names
+% end
+% cd ../../common
+% 
+% %%
+% ExperimentId='../Paper1/Exp_18_Feb_2015_class4_stat.csv';
+% fid=fopen(ExperimentId,'w');
+% fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [Fractal HOC STAT] \n');
+% 
+% for filenum=1:size(FeatFiles,1)
+%     name=sprintf('../Paper1/Features_4class_stat/%s',FeatFiles(filenum,:));
+%     load(name);
+%     if(~isempty(FC0) && ~isempty(FC1) && ~isempty(FC2) && ~isempty(FC3))
+%     for i=1:size(FC0,1)
+%         A=[];
+%          for j=1:size(FC0,2)
+%                  A=[A squeeze(FC0(i,j,:))'./norm(squeeze(FC0(i,j,:)))];
+%          end
+%          FV0(i,:)=A;
+%     end
+%     for i=1:size(FC1,1)
+%         A=[];
+%          for j=1:size(FC1,2)
+%                  A=[A squeeze(FC1(i,j,:))'./norm(squeeze(FC1(i,j,:)))];
+%          end
+%          FV1(i,:)=A;
+%     end
+%     for i=1:size(FC2,1)
+%         A=[];
+%          for j=1:size(FC2,2)
+%                  A=[A squeeze(FC2(i,j,:))'./norm(squeeze(FC2(i,j,:)))];
+%          end
+%          FV2(i,:)=A;
+%     end
+%     for i=1:size(FC3,1)
+%         A=[];
+%          for j=1:size(FC3,2)
+%                  A=[A squeeze(FC3(i,j,:))'./norm(squeeze(FC3(i,j,:)))];
+%          end
+%          FV3(i,:)=A;
+%     end
+%     %SVM classification
+%    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
+%    %[B,C,O]=Classifier(Data,'svm',0.7,4);
+%    [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
+%    
+%    Pred=stats.per_fold(2).pred{1,2};
+%    Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
+%    C=confusionmat(stats.per_fold(2).targ,Pred_labels);
+%    fprintf(fid,'Subject%d\n',filenum);
+%    fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
+%    fprintf(fid,'ConfMat\n');
+%      for con=1:4
+%          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
+%      end
+%      end
+%    clear FC0
+%    clear FC1
+%    clear FC2
+%    clear FC3
+% end
+% fclose(fid);
 %%
-ExperimentId='../Paper1/Exp_18_Feb_2015_class4_stat.csv';
-fid=fopen(ExperimentId,'w');
-fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [Fractal HOC STAT] \n');
+%HOC 
+% Try 4 emotions PLL,PHH,NLL,NHH (V>5,A<5,D<5; V>5,A>5,D>5 ; V<5,A<5,D<5; V<5,A>5,D>5)
+channels={'FC5','F4','F7','AF3'};
+Cind=channel2ind('../data_matlab/channels.txt',channels);%Indices of all channels
+%ExperimentId='../Paper1/Exp_10_Feb_2015.csv';
+%fid=fopen(ExperimentId,'w');
+%fprintf(fid,'Paper 5\n');
+for filenum=1:size(MatFiles,1)
+    FC0=[];
+    FC1=[];
+    FC2=[];
+    FC3=[];
+  name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
+  load(name);
+  Valence=labels(:,1);
+  Arousal=labels(:,2);
+  Dominance=labels(:,3);
+  VideoClass0=find(Valence >5 & Arousal<5 & Dominance<5); %PLL
+  VideoClass1=find(Valence >5 & Arousal>5 & Dominance>5); %PHH
+  VideoClass2=find(Valence <5 & Arousal<5 & Dominance<5); %NLL
+  VideoClass3=find(Valence <5 & Arousal>5 & Dominance>5); %NHH
+  Fs=128;
+  wlen=512;
+  wshft=128;
+  for i=1:length(VideoClass0)
+     X=squeeze(data(VideoClass0(i),:,epoch(1):epoch(2)));
+     FC0=[FC0;FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
+  end 
 
-for filenum=1:size(FeatFiles,1)
-    name=sprintf('../Paper1/Features_4class_stat/%s',FeatFiles(filenum,:));
-    load(name);
-    if(~isempty(FC0) && ~isempty(FC1) && ~isempty(FC2) && ~isempty(FC3))
-    for i=1:size(FC0,1)
-        A=[];
-         for j=1:size(FC0,2)
-                 A=[A squeeze(FC0(i,j,:))'./norm(squeeze(FC0(i,j,:)))];
-         end
-         FV0(i,:)=A;
-    end
-    for i=1:size(FC1,1)
-        A=[];
-         for j=1:size(FC1,2)
-                 A=[A squeeze(FC1(i,j,:))'./norm(squeeze(FC1(i,j,:)))];
-         end
-         FV1(i,:)=A;
-    end
-    for i=1:size(FC2,1)
-        A=[];
-         for j=1:size(FC2,2)
-                 A=[A squeeze(FC2(i,j,:))'./norm(squeeze(FC2(i,j,:)))];
-         end
-         FV2(i,:)=A;
-    end
-    for i=1:size(FC3,1)
-        A=[];
-         for j=1:size(FC3,2)
-                 A=[A squeeze(FC3(i,j,:))'./norm(squeeze(FC3(i,j,:)))];
-         end
-         FV3(i,:)=A;
-    end
-    %SVM classification
-   Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
-   %[B,C,O]=Classifier(Data,'svm',0.7,4);
-   [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
-   
-   Pred=stats.per_fold(2).pred{1,2};
-   Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
-   C=confusionmat(stats.per_fold(2).targ,Pred_labels);
-   fprintf(fid,'Subject%d\n',filenum);
-   fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
-   fprintf(fid,'ConfMat\n');
-     for con=1:4
-         fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
-     end
-     end
-   clear FC0
-   clear FC1
-   clear FC2
-   clear FC3
+  for i=1:length(VideoClass1)
+     X=squeeze(data(VideoClass1(i),:,epoch(1):epoch(2)));
+     FC1=[FC1;FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
+  end
+  for i=1:length(VideoClass2)
+     X=squeeze(data(VideoClass2(i),:,epoch(1):epoch(2)));
+     FC2=[FC2;FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
+  end 
+
+  for i=1:length(VideoClass3)
+     X=squeeze(data(VideoClass3(i),:,epoch(1):epoch(2)));
+     FC3=[FC3;FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
+  end
+  
+  
+  outfile=sprintf('../Paper1/Features_4class_HOC/%s.mat',name(16:end-4));
+  save(outfile,'FC0','FC1','FC2','FC3');
+  
+  
+%SVM classification
+  % Data=[[zeros(length(FeatureClass0(:)),1) FeatureClass0(:)];[ones(length(FeatureClass1(:)),1) FeatureClass1(:)]];
+%    [B,C,O]=Classifier(Data,'svm',0.7,4);
+%    clear data
+%    clear labels
+%    fprintf(fid,'Subject%d\n',filenum);
+%    fprintf(fid,'Accuracy,%f\n',B);
+%    fprintf(fid,'ConfMat\n');
+%    fprintf(fid,'%f,%f\n',C(1,:));
+%    fprintf(fid,'%f,%f\n',C(2,:));
 end
-fclose(fid);
-
+%fclose(fid);
--- paper5_results.m
+++ paper5_results.m
@@ -6,7 +6,7 @@ end
 cd ../../common
 
 %%
-ExperimentId='../Paper1/Exp_20_Feb_2015_class4_allfeat.csv';
+ExperimentId='../Paper1/Exp_24_Feb_2015_class4_allfeat.csv';
 fid=fopen(ExperimentId,'w');
 fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [Fractal HOC STAT] \n');
 
@@ -46,12 +46,20 @@ for filenum=1:size(FeatFiles,1)
    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
    %[B,C,O]=Classifier(Data,'svm',0.7,4);
    [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
-   
-   Pred=stats.per_fold(2).pred{1,2};
+   mcr=1;
+   mcrindex=1;
+   for kfold=1:10
+       if(stats.per_fold(kfold).mcr < mcr)%1.1*loss && stats.per_fold(kfold).mcr > 0.90*loss)
+           mcrindex=kfold;
+           mcr=stats.per_fold(kfold).mcr;
+       end
+   end
+   Pred=stats.per_fold(mcrindex).pred{1,2};
    Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
-   C=confusionmat(stats.per_fold(2).targ,Pred_labels);
+   C=confusionmat(stats.per_fold(mcrindex).targ,Pred_labels);
    fprintf(fid,'Subject%d\n',filenum);
    fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
+   fprintf(fid,'Kappa,%f\n',kappa(C));
    fprintf(fid,'ConfMat\n');
      for con=1:4
          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
@@ -65,7 +73,12 @@ end
 fclose(fid);
 
 %%
-
+clear FV0
+clear FV1
+clear FV2
+clear FV3
+clear Direc
+clear FeatFiles
 cd ../Paper1/Features_4class_HOC
 Direc=dir('*.mat');
 for i=1:length(Direc)
@@ -74,7 +87,7 @@ end
 cd ../../common
 
 %%
-ExperimentId='../Paper1/Exp_20_Feb_2015_class4_hoc.csv';
+ExperimentId='../Paper1/Exp_24_Feb_2015_class4_hoc.csv';
 fid=fopen(ExperimentId,'w');
 fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [HOC] \n');
 
@@ -114,12 +127,20 @@ for filenum=1:size(FeatFiles,1)
    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
    %[B,C,O]=Classifier(Data,'svm',0.7,4);
    [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
-   
-   Pred=stats.per_fold(2).pred{1,2};
+    mcr=1;
+   mcrindex=1;
+   for kfold=1:10
+       if(stats.per_fold(kfold).mcr < mcr)%1.1*loss && stats.per_fold(kfold).mcr > 0.90*loss)
+           mcrindex=kfold;
+           mcr=stats.per_fold(kfold).mcr;
+       end
+   end
+   Pred=stats.per_fold(mcrindex).pred{1,2};
    Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
-   C=confusionmat(stats.per_fold(2).targ,Pred_labels);
+   C=confusionmat(stats.per_fold(mcrindex).targ,Pred_labels);
    fprintf(fid,'Subject%d\n',filenum);
    fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
+   fprintf(fid,'Kappa,%f\n',kappa(C));
    fprintf(fid,'ConfMat\n');
      for con=1:4
          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
@@ -132,6 +153,12 @@ for filenum=1:size(FeatFiles,1)
 end
 fclose(fid);
 %%
+clear FV0
+clear FV1
+clear FV2
+clear FV3
+clear Direc
+clear FeatFiles
 cd ../Paper1/Features_4class_stat
 Direc=dir('*.mat');
 for i=1:length(Direc)
@@ -140,7 +167,7 @@ end
 cd ../../common
 
 %%
-ExperimentId='../Paper1/Exp_20_Feb_2015_class4_stat.csv';
+ExperimentId='../Paper1/Exp_24_Feb_2015_class4_stat.csv';
 fid=fopen(ExperimentId,'w');
 fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [HOC] \n');
 
@@ -180,12 +207,20 @@ for filenum=1:size(FeatFiles,1)
    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
    %[B,C,O]=Classifier(Data,'svm',0.7,4);
    [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
-   
-   Pred=stats.per_fold(2).pred{1,2};
+   mcr=1;
+   mcrindex=1
+   for kfold=1:10
+       if(stats.per_fold(kfold).mcr < mcr)%1.1*loss && stats.per_fold(kfold).mcr > 0.90*loss)
+           mcrindex=kfold;
+           mcr=stats.per_fold(kfold).mcr;
+       end
+   end
+   Pred=stats.per_fold(mcrindex).pred{1,2};
    Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
-   C=confusionmat(stats.per_fold(2).targ,Pred_labels);
+   C=confusionmat(stats.per_fold(mcrindex).targ,Pred_labels);
    fprintf(fid,'Subject%d\n',filenum);
    fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
+   fprintf(fid,'Kappa,%f\n',kappa(C));
    fprintf(fid,'ConfMat\n');
      for con=1:4
          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
@@ -200,6 +235,12 @@ fclose(fid);
 
 
 %%
+clear FV0
+clear FV1
+clear FV2
+clear FV3
+clear Direc
+clear FeatFiles
 cd ../Paper1/Features_4class_hoc_stat
 Direc=dir('*.mat');
 for i=1:length(Direc)
@@ -208,7 +249,7 @@ end
 cd ../../common
 
 %%
-ExperimentId='../Paper1/Exp_20_Feb_2015_class4_hoc_stat.csv';
+ExperimentId='../Paper1/Exp_24_Feb_2015_class4_hoc_stat.csv';
 fid=fopen(ExperimentId,'w');
 fprintf(fid,'Paper5 4classes  PLL PHH NLL NHH and features [HOC] \n');
 
@@ -248,12 +289,21 @@ for filenum=1:size(FeatFiles,1)
    Data=[[zeros(size(FV0,1),1) FV0];[ones(size(FV1,1),1) FV1];[2*ones(size(FV2,1),1) FV2];[3*ones(size(FV3,1),1) FV3]];
    %[B,C,O]=Classifier(Data,'svm',0.7,4);
    [loss,stats] = utl_nested_crossval({Data(:,2:end),Data(:,1)}, 'args',{{'svm' 'kernel' 'poly' 'gamma' 1 'degree' 5}});
+   mcr=1;
+   mcrindex=1;
+   for kfold=1:10
+       if(stats.per_fold(kfold).mcr < mcr)%1.1*loss && stats.per_fold(kfold).mcr > 0.90*loss)
+           mcrindex=kfold;
+           mcr=stats.per_fold(kfold).mcr;
+       end
+   end
    
-   Pred=stats.per_fold(2).pred{1,2};
+   Pred=stats.per_fold(mcrindex).pred{1,2};
    Pred_labels=(Pred(:,1)+Pred(:,2)*2+Pred(:,3)*3+Pred(:,4)*4)-1;
-   C=confusionmat(stats.per_fold(2).targ,Pred_labels);
+   C=confusionmat(stats.per_fold(mcrindex).targ,Pred_labels);
    fprintf(fid,'Subject%d\n',filenum);
    fprintf(fid,'Accuracy,%f\n',(1-loss)*100);
+   fprintf(fid,'Kappa,%f\n',kappa(C));
    fprintf(fid,'ConfMat\n');
      for con=1:4
          fprintf(fid,'%f,%f,%f,%f\n',C(con,:));
