c3043593065761cc4145fdeba64184adfc5b8a0d
rahulmanocha1990 <rahulmanocha1990@tamu.edu>
2015-04-06 16:00:16 -0500

    may not be used

--- genstorefeatures.m
+++ genstorefeatures.m
@@ -20,54 +20,7 @@ Cind=1:32;
 Fs=128;
 wlen=512;
 wshft=128;
-%%
-% for filenum=1:size(MatFiles,1)
-%   name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
-%   load(name);
-%   Valence=labels(:,1);
-%   Arousal=labels(:,2);
-%   Dominance=labels(:,3);
-%   VideoClass=sortclassindices(Arousal,Valence,Dominance,classes);
-%   FeatVect=cell(length(VideoClass),2);
-%   for i=1:length(VideoClass)
-%       FeatVect{i,1}=VideoClass{i,1};
-%       indices=VideoClass{i,2};
-%       for j=1:length(indices)
-%         X=squeeze(data(indices(j),:,1:8096-384));
-%         FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8)];
-%       end
-%   end 
-%   outfile=sprintf('../Paper1/Features_Fractal/%s.mat',name(16:end-4));
-%   save(outfile,'FeatVect');
-% end
-%%
-% for filenum=1:size(MatFiles,1)
-%   name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
-%   load(name);
-%   Valence=labels(:,1);
-%   Arousal=labels(:,2);
-%   Dominance=labels(:,3);
-%   VideoClass=sortclassindices(Arousal,Valence,Dominance,classes);
-%   FeatVect=cell(length(VideoClass),2);
-%   FeatVect2=cell(length(VideoClass),2);
-%   for i=1:length(VideoClass)
-%       FeatVect{i,1}=VideoClass{i,1};
-%       FeatVect2{i,1}=VideoClass{i,1};
-%       indices=VideoClass{i,2};
-%       for j=1:length(indices)
-%         X=squeeze(data(indices(j),:,1:8096-384));
-%         FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
-%         FeatVect2{i,2}=[FeatVect2{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
-%       end
-%   end 
-%   outfile=sprintf('../Paper1/Features_HOC/%s.mat',name(16:end-4));
-%   save(outfile,'FeatVect');
-%   outfile2=sprintf('../Paper1/Features_STAT/%s.mat',name(16:end-4));
-%   save(outfile2,'FeatVect2');
-%   
-% end
-
-%%
+epoch=[(128*6)+1,8064];
 %%
 for filenum=1:size(MatFiles,1)
   name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
@@ -81,12 +34,60 @@ for filenum=1:size(MatFiles,1)
       FeatVect{i,1}=VideoClass{i,1};
       indices=VideoClass{i,2};
       for j=1:length(indices)
-        X=squeeze(data(indices(j),:,1:8096-384));
-        FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'PowSpec',{4,7,'-',8,13,'-',14,29,'-',30,47})];
+        X=squeeze(data(indices(j),:,epoch(1):epoch(2)));
+        FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'Fractal',8)];
       end
   end 
-  outfile=sprintf('../Paper1/Features_BinPow/%s.mat',name(16:end-4));
+  outfile=sprintf('../Paper1/Features_Fractal/%s.mat',name(16:end-4));
   save(outfile,'FeatVect');
 end
+%
+for filenum=1:size(MatFiles,1)
+  name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
+  load(name);
+  Valence=labels(:,1);
+  Arousal=labels(:,2);
+  Dominance=labels(:,3);
+  VideoClass=sortclassindices(Arousal,Valence,Dominance,classes);
+  FeatVect=cell(length(VideoClass),2);
+  FeatVect2=cell(length(VideoClass),2);
+  for i=1:length(VideoClass)
+      FeatVect{i,1}=VideoClass{i,1};
+      FeatVect2{i,1}=VideoClass{i,1};
+      indices=VideoClass{i,2};
+      for j=1:length(indices)
+        X=squeeze(data(indices(j),:,1:8096-384));
+        FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'HOC',5)];
+        FeatVect2{i,2}=[FeatVect2{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'STAT','msdefg')];
+      end
+  end 
+  outfile=sprintf('../Paper1/Features_HOC/%s.mat',name(16:end-4));
+  save(outfile,'FeatVect');
+  outfile2=sprintf('../Paper1/Features_STAT/%s.mat',name(16:end-4));
+  save(outfile2,'FeatVect2');
+  
+end
+
+%%
+%%
+% for filenum=1:size(MatFiles,1)
+%   name=sprintf('../data_matlab/%s',MatFiles(filenum,:));
+%   load(name);
+%   Valence=labels(:,1);
+%   Arousal=labels(:,2);
+%   Dominance=labels(:,3);
+%   VideoClass=sortclassindices(Arousal,Valence,Dominance,classes);
+%   FeatVect=cell(length(VideoClass),2);
+%   for i=1:length(VideoClass)
+%       FeatVect{i,1}=VideoClass{i,1};
+%       indices=VideoClass{i,2};
+%       for j=1:length(indices)
+%         X=squeeze(data(indices(j),:,1:8096-384));
+%         FeatVect{i,2}=[FeatVect{i,2};FeatureGen(X,Cind,wlen,wshft,Fs,'PowSpec',{4,7,'-',8,13,'-',14,29,'-',30,47})];
+%       end
+%   end 
+%   outfile=sprintf('../Paper1/Features_BinPow/%s.mat',name(16:end-4));
+%   save(outfile,'FeatVect');
+% end
 
 
--- visualize_features.m
+++ visualize_features.m
@@ -3,65 +3,79 @@
 
 load('../data_matlab/biosemi32chnn.mat');
 %% --Load feature vector files
-cd ../Paper1/Features_Fractal
-Direc=dir('*.mat');
-for i=1:length(Direc)
-FeatFiles(i,:)=Direc(i).name;%get all file names
-end
-cd ../
+% cd ../Paper1/Features_Fractal
+% Direc=dir('*.mat');
+% for i=1:length(Direc)
+% FeatFiles(i,:)=Direc(i).name;%get all file names
+% end
+% cd ../
+% %%
+% for filenum=1:size(FeatFiles,1)
+%     name=sprintf('Features_Fractal/%s',FeatFiles(filenum,:));
+%     load(name);
+%     
+%     classes=size(FeatVect,1);
+%     for clas=1:classes
+%         dat=squeeze(FeatVect{clas,2});
+%         L=size(dat,1);
+%         avg_size=floor(L/5);
+%         count=1;
+%         classname=FeatVect{clas,1};
+%         for i=1:avg_size:L-avg_size
+%             handl=figure;topoplot(rescale(mean(dat(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
+%             title(classname,'FontSize',16);
+%             plotname=sprintf('Features_Fractal/spatial_patt/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),classname,count);
+%             saveas(handl,plotname);
+%             count=count+1;
+%             close(handl);
+%         end
+%     end
+% end
 %%
-for filenum=1:size(FeatFiles,1)
-    name=sprintf('Features_Fractal/%s',FeatFiles(filenum,:));
-    load(name);
-    
-    classes=size(FeatVect,1);
-    for clas=1:classes
-        dat=squeeze(FeatVect{clas,2});
-        L=size(dat,1);
-        avg_size=floor(L/5);
-        count=1;
-        classname=FeatVect{clas,1};
-        for i=1:avg_size:L-avg_size
-            handl=figure;topoplot(rescale(mean(dat(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
-            title(classname,'FontSize',16);
-            plotname=sprintf('Features_Fractal/spatial_patt/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),classname,count);
-            saveas(handl,plotname);
-            count=count+1;
-            close(handl);
-        end
-    end
-end
-%%
-
-for filenum=1:size(FeatFiles,1)
-    name=sprintf('Features_BinPow/%s',FeatFiles(filenum,:));
-    load(name);
-    
-    classes=size(FeatVect,1);
-    for clas=1:classes
-        dat=FeatVect{clas,2};
-        L=size(dat,1);
-        avg_size=floor(L/5);
-        count=1;
-        classname=FeatVect{clas,1};
-        P1=squeeze(dat(:,1,:));
-        P2=squeeze(dat(:,2,:));
-        P3=squeeze(dat(:,3,:));
-        P4=squeeze(dat(:,4,:));
-        for i=1:avg_size:L-avg_size
-            handl=figure;
-            subplot(1,4,1);topoplot(rescale(mean(P1(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
-            subplot(1,4,2);topoplot(rescale(mean(P2(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
-            subplot(1,4,3);topoplot(rescale(mean(P3(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
-            subplot(1,4,4);topoplot(rescale(mean(P4(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
-            title(classname,'FontSize',16);
-            plotname=sprintf('Features_BinPow/spatial_patt/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),classname,count);
-            saveas(handl,plotname);
-            count=count+1;
-            close(handl);
-        end
-    end
-end
+% cd ../Paper1/Features_BinPow
+% Direc=dir('*.mat');
+% for i=1:length(Direc)
+% FeatFiles(i,:)=Direc(i).name;%get all file names
+% end
+% cd ../
+% %%
+% for filenum=1:size(FeatFiles,1)
+%     name=sprintf('Features_BinPow/%s',FeatFiles(filenum,:));
+%     load(name);
+%     
+%     classes=size(FeatVect,1);
+%     for clas=1:classes
+%         dat=FeatVect{clas,2};
+%         L=size(dat,1);
+%         avg_size=floor(L/5);
+%         count=1;
+%         classname=FeatVect{clas,1};
+%         P1=squeeze(dat(:,1,:));
+%         P2=squeeze(dat(:,2,:));
+%         P3=squeeze(dat(:,3,:));
+%         P4=squeeze(dat(:,4,:));
+%         for i=1:avg_size:L-avg_size
+%             handl=figure;
+%             subplot(1,4,1);topoplot(rescale(mean(P1(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
+%             title('4-7Hz','FontWeight','bold');
+%             subplot(1,4,2);topoplot(rescale(mean(P2(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
+%             title('8-13Hz','FontWeight','bold');
+%             subplot(1,4,3);topoplot(rescale(mean(P3(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
+%             title('14-29Hz','FontWeight','bold');
+%             subplot(1,4,4);topoplot(rescale(mean(P4(i:i+avg_size,:)),1,-1),chann,'electrodes','labels');
+%             title('30-47Hz','FontWeight','bold');
+%             ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off',...
+%                 'Visible','off','Units','normalized', 'clipping' , 'off');
+%             text(0.5, 0.8,classname,'HorizontalAlignment' ,'center','VerticalAlignment', 'top',...
+%                 'FontSize',15);
+%            % title(classname,'FontSize',16);
+%             plotname=sprintf('Features_BinPow/spatial_patt/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),classname,count);
+%             saveas(handl,plotname);
+%             count=count+1;
+%             close(handl);
+%         end
+%     end
+% end
 
 %%
 %% --Load bcilab mat files
@@ -70,18 +84,18 @@ Direc=dir('s*.mat');
 for i=1:length(Direc)
 FeatFiles(i,:)=Direc(i).name;%get all file names
 end
-cd ../
+cd ../Paper1
 %%
 for filenum=1:size(FeatFiles,1)
-     name=sprintf('Features_BinPow/%s',FeatFiles(filenum,:));
+     name=sprintf('../bcilab_matfiles/%s',FeatFiles(filenum,:));
      EEG=load(name);
      count=1;
      for nevent=1:40
        handl=figure;
-       [spectra,freqs]=spectopo(EEG.data(:,EEG.event(nevent).latency:8064+EEG.event(nevent).latency),0,128,...
+       [spectra,freqs]=spectopo(EEG.data(:,EEG.event(nevent).latency:8063+EEG.event(nevent).latency),0,128,...
            'freq',[4,8,13,20,40],'chanlocs',chann,'title',EEG.event(nevent).type,'winsize',1024,...
            'overlap',512,'rmdc','on');
-       plotname=sprintf('Spectopo_plots/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),EEG.event(nevent).type,count);
+       plotname=sprintf('spectopo_plots/%s_%s_%d.fig',FeatFiles(filenum,1:end-4),EEG.event(nevent).type,count);
        saveas(handl,plotname);
        count=count+1;
        close(handl);
